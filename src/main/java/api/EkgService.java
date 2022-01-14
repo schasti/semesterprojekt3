e16cab2f236f886c84f8a659afe91e5e02e86163
Naveed <imamshah19@yahoo.com>
2022-01-14 11:33:48 +0100

    udpensle data fra json object.

--- src/main/java/api/EkgService.java
+++ src/main/java/api/EkgService.java
@@ -1,10 +1,13 @@
 package api;
 
+import com.google.gson.Gson;
 import controller.EkgController;
 import model.EkgData;
+import org.apache.http.HttpHeaders;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +21,16 @@ public class EkgService {
     @Produces({MediaType.APPLICATION_JSON})
     @Consumes({MediaType.APPLICATION_JSON})
     public EkgData ekgpython(EkgData data){
-       // EkgData ekgData = new EkgData();
-      // EkgController.getEkgControllerObj().setS(data);
+
+        System.out.println("hej hej    "+data.getData()+"      farvel    farvel ");
+
         return data;
     }
+
+
+
    /* public EkgData ekgpython(@QueryParam("identifier") String cpr,@QueryParam("session") String session, String data){
-        EkgData ekgdata = new EkgData();
+
 
         ekgdata.setCpr(cpr);
         ekgdata.setSession(Integer.parseInt(session));
@@ -31,6 +38,7 @@ public class EkgService {
         ekgdata.setDatanotlist(data);
         return ekgdata ;
     }*/
+
 
     @GET
     @Consumes({MediaType.APPLICATION_JSON})
--- src/main/java/model/EkgData.java
+++ src/main/java/model/EkgData.java
@@ -1,20 +1,37 @@
 package model;
 
+import org.apache.http.HttpHeaders;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class EkgData {
 
 
-    public List<Float> getData() {
-        return data;
-    }
-
-    public void setData(List<Float> data) {
-        this.data = data;
-    }
-
     private List<Float> data;
+
+
+
+    public List<Float> getData() {return data;}
+    public void setData(List<Float> data) {this.data = data;}
+
+
+
+    public void findHeaders(HttpHeaders httpHeaders){
+        String cpr = String.valueOf(httpHeaders);
+        System.out.println(cpr);
+    }
+/*
+    private String cpr;
+    private int sessionID;
+    public String getCpr() {return cpr;}
+    public void setCpr(String cpr) {this.cpr = cpr;}
+
+
+
+    public int getSessionID() {return sessionID;}
+    public void setSessionID(int sessionID) {this.sessionID = sessionID;}
+*/
 
 
 
