fa8c6ce6487556dff271ce73a475eeea5bc9799c
Naveed <imamshah19@yahoo.com>
2022-01-17 15:20:09 +0100

    indhente ekgsessionlist done ish

--- src/main/java/dataAccesLayer/SQL.java
+++ src/main/java/dataAccesLayer/SQL.java
@@ -193,7 +193,7 @@ public class SQL {
 
             PreparedStatement pp = myConn.prepareStatement(
 
-                    "CREATE TABLE sp3.cpr"+ekgSession.getCpr()+"session"+ekgSession.getSession()+" (measurement INT NOT NULL AUTO_INCREMENT,"+"value VARCHAR(45) NULL,"+" PRIMARY KEY (measurement))"
+                    "CREATE TABLE sp3."+"session"+ekgSession.getSession()+" (measurement INT NOT NULL AUTO_INCREMENT,"+"value VARCHAR(45) NULL,"+" PRIMARY KEY (measurement))"
             );
             pp.execute();
             removeConnectionSQL();
@@ -201,7 +201,7 @@ public class SQL {
 
         } catch (SQLException throwables) {
             OurException ex = new OurException();
-            ex.setMessage("Denne person har haft den angivede session");
+            ex.setMessage("Denne Session findes allerede");
             try {
                 throw ex;
             } catch (OurException e) {
@@ -214,10 +214,14 @@ public class SQL {
         try {
             makeConnectionSQL();
 
-                for (int i = 0; i < ekgData.getData().size() - 1; i ++) {
-                    String write_to_measurement = "INSERT INTO sp3.cpr"+ekgSession.getCpr()+"session"+ekgSession.getSession()+" (value) values(?);";
+                for (int i = 0; i < ekgData.getData().size() - 1; i +=5) {
+                    String write_to_measurement = "INSERT INTO sp3.cpr"+ekgSession.getCpr()+"session"+ekgSession.getSession()+" (value) values(?)+(?)+(?)+(?)+(?);";
                     PreparedStatement PP = myConn.prepareStatement(write_to_measurement);
                     PP.setString(1, String.valueOf(ekgData.getData().get(i)));
+                    PP.setString(1, String.valueOf(ekgData.getData().get(i+1)));
+                    PP.setString(1, String.valueOf(ekgData.getData().get(i+2)));
+                    PP.setString(1, String.valueOf(ekgData.getData().get(i+3)));
+                    PP.setString(1, String.valueOf(ekgData.getData().get(i+4)));
                     PP.execute();
                 }
                 System.out.println("Done SQL");
