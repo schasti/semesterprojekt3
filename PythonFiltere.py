4c16a6d64af487e58ab4e4b92f2a470dd9aac14a
ZAkir Hussain Shahoo <s194054@student.dtu.dk>
2022-01-21 18:29:18 +0100

    Python file med navn "PythinFilter.py" med

--- PythonFiltere.py
+++ PythonFiltere.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 12 19:40:03 2022
+For at køre python filteret så skal man ligge filen
+ind i Person_1 EKG Måling
 
 @author: bruger
 """
